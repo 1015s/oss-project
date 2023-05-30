e7ac0e50d9ceae2645bb5d2cce9953ac37ea56b0
sueun lee <1015_s@naver.com>
2023-05-30 17:32:28 +0900

    [FEAT] Feat1, Feat2 구현

--- filebrowser.py
+++ filebrowser.py
@@ -1752,8 +1752,116 @@ class FileBrowser(tk.Toplevel):
         except subprocess.CalledProcessError as e:
             messagebox.showinfo("File Status", f"Git commit failed: {e}")
             return False
+    
+    ### Branch Management
+    
+    # input : folder_path -> output : 현재 branch 이름 (git에 의해 관리되지 않으면 None 반환)
+    def current_branch(self, folder_path):
+        if not self.check_git_managed(folder_path):
+            return None
+        else:
+            cmd = ["git", "branch", "--show-current"]
+            result = subprocess.run(cmd, cwd=folder_path, capture_output=True, text=True)
+            branch_name = result.stdout.strip()
+            return branch_name
+    
+    # input : 새로운 branch 이름, folder path -> 새로운 branch 생성
+    def branch_create(self, folder_path, branch_name):
+        try:
+            cmd = ["git", "branch", branch_name]
+            subprocess.run(cmd, cwd=folder_path)
+            messagebox.showinfo("Branch Status", f"Branch '{branch_name}' created successfully.")
+            return True
+        except Exception as e:
+            messagebox.showinfo("Branch Status", f"An error occurred: {e}")
+            return False
+    
+    # input : folder_path -> output : 현재 git repo에 있는 branch 이름 list 반환(git에 의해 관리되지 않으면 빈 list 반환)
+    def branch_list(self, folder_path):
+        if not self.check_git_managed(folder_path):
+            return []
+        else:
+            cmd = ["git", "branch"]
+            result = subprocess.check_output(cmd, cwd=folder_path, universal_newlines=True)
+            branches = [branch.strip() for branch in result.splitlines()]
+            return branches
+    
+    # input : folder_path -> output : current branch 제외한 나머지 branch list 반환
+    def get_other_branches(self, folder_path):
+        current_branch = self.current_branch(folder_path)
+        branches = self.branch_list(folder_path)
         
+        other_branches = [branch for branch in branches if branch != current_branch]
+        return other_branches
+    
+    # input : folder_path, 삭제하고 싶은 branch 이름 -> output : 해당 branch가 있으면 삭제, 없으면 오류 메시지
+    def branch_delete(self, folder_path, branch_name):
+        try:
+            cmd = ["git", "branch", "-D", branch_name]
+            subprocess.run(cmd, cwd=folder_path)
+            messagebox.showinfo("Branch Status", f"The branch '{branch_name}' has been deleted.")
+            return True
+        except subprocess.CalledProcessError as e:
+            messagebox.showinfo("Branch Status", f"The branch '{branch_name}' does not exist.")
+            return False
 
+    
+    # input : folder_path, 기존 branch 이름, 새로운 branch 이름 -> output : 기존 branch 이름이 존재하면 rename, 없으면 오류 메시지
+    def branch_rename(self, folder_path, old_branch_name, new_branch_name):
+        try:
+            cmd = ["git", "branch", "-m", old_branch_name, new_branch_name]
+            subprocess.run(cmd, cwd=folder_path)
+            messagebox.showinfo("Branch Status", f"The branch '{old_branch_name}' has been renamed to '{new_branch_name}'.")
+            return True
+        except subprocess.CalledProcessError as e:
+            messagebox.showinfo("Branch Status", f"Failed to rename the branch '{old_branch_name}'.")
+            return False
+    
+    # input : folder_path, 바꾸고 싶은 branch 이름 -> output : 있으면 checkout, 없으면 오류 메시지
+    def branch_checkout(self, folder_path, branch_name):
+        try:
+            cmd = ["git", "checkout", branch_name]
+            subprocess.run(cmd, cwd=folder_path)
+            messagebox.showinfo("Branch Status", f"Switched to the branch '{branch_name}'.")
+            return True
+        except subprocess.CalledProcessError as e:
+            messagebox.showinfo("Branch Status", f"Failed to switch to the branch '{branch_name}'.")
+            return False
+    
+    
+    ### Branch Merge
+    
+    # input : folder_path, git merge 하고 싶은 branch 이름 
+    # output : success 이면 True, error 이면 git merge abort 하고, False와 unmerged paths 담은 messagebox 띄움
+    def merge_branch(self, folder_path, branch_name):
+        # 해당 폴더가 git repository에 연결되어 있는지 확인
+        if not self.check_git_managed(folder_path):
+            return False
+
+        try:
+            # git merge 수행
+            cmd_merge = ["git", "merge", branch_name]
+            subprocess.run(cmd_merge, cwd=folder_path)
+
+            # merge 중에 발생한 충돌 또는 에러 확인
+            cmd_status = ["git", "status", "-s", "--unmerged"]
+            result = subprocess.check_output(cmd_status, cwd=folder_path, universal_newlines=True)
+            unmerged_paths = [line.split()[1] for line in result.splitlines()]
+
+            if unmerged_paths:
+                subprocess.run(["git", "merge", "--abort"], cwd=folder_path)
+                messagebox.showinfo("Git Status", f"Merge aborted due to conflicts or errors.\n\nUnmerged paths: {unmerged_paths}")
+                return False
+            else:
+                messagebox.showinfo("Git Status", "Merge completed successfully.")
+                return True
+
+        except subprocess.CalledProcessError as e:
+            messagebox.showinfo("Git Status", f"Merge failed with error: {e}")
+            return False    
+    
+    
+        
     #우클릭시
 
     def _select_rightmouse(self, event):    
